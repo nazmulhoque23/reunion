be94468fd6d5277652cd827c753840a6b19ad1d5
nazmulhoque23 <hoque23kazi@gmail.com>
2022-10-24 23:43:14 +0530

    Update pay.blade.php

--- resources/views/layouts/pay.blade.php
+++ resources/views/layouts/pay.blade.php
@@ -22,7 +22,7 @@
                         
                     </div>
                     <div class="col-md-3"></div>
-                        @include('layouts.finalform')
+                       
                     </div>
                 
             </div>
